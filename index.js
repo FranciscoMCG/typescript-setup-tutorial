75faf8a220402d063f8e910c7c7794939fe88d79
Francisco Gomes <franciscogomestv@gmail.com>
2019-10-21 23:31:20 +0100

    reset done

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.__esModule = true;
+console.log("Hello World");
+var name = "Francisco";
+var age = 36;
+var character = function (name, age) {
+    console.log("My name is " + name + ". My age is " + age);
+};
---
+++ index.ts
@@ -0,0 +1,10 @@
+export {}
+
+console.log("Hello World")
+
+const name = "Francisco"
+const age = 36
+
+const character = (name: string, age: number): void => {
+  console.log(`My name is ${name}. My age is ${age}`)
+}
